892586124c9a750bc8a1f8ef534bdd7e932faaac
SumitSharma <sumitkumarsharma2748@gmail.com>
2026-02-02 01:54:19 +0530

    added download and preview functionality

--- 08_Drive/Server/server.js
+++ 08_Drive/Server/server.js
@@ -1,33 +1,59 @@
+import { log } from "console";
 import { open, readdir, readFile } from "fs/promises";
 import http from "http";
+import mime from "mime-types";
 
 async function severDirectory(req, res, BoilerplateCode) {
-  const allAssets = await readdir("./storage" + req.url);
+  const [url, queryString] = req.url.split("?");
+  const allAssets = await readdir(`./storage${url}`, { withFileTypes: true });
   let dynamicHTML = "";
-
   allAssets.forEach((asset) => {
-    dynamicHTML += `<a href=".${req.url === "/" ? "" : req.url}/${asset}">${asset}</a><br>`;
+    const isDir = asset.isDirectory();
+    const name = asset.name;
+    dynamicHTML += `<div>${name}
+            <a href="${url === "/" ? "" : url}/${name}?action=open&test=1234">Open</a>
+             ${isDir ? "" : `<a href="${url === "/" ? "" : url}/${name}?action=download">Download</a>`}
+        </div>`;
   });
-
   return res.end(BoilerplateCode.replace("${dynamicHTML}", dynamicHTML));
 }
 
 const server = http.createServer(async (req, res) => {
   const BoilerplateCode = await readFile("./index.html", "utf-8");
-  if (req.url === "/favicon.ico") return;
-
+  if (req.url === "/favicon.ico") return res.end("fevicon is not available!");
   if (req.url === "/") {
     await severDirectory(req, res, BoilerplateCode);
   } else {
+    const [url, queryString] = req.url.split("?");
+    console.log({ url, queryString });
+
+    const querryParams = {};
+    if (queryString) {
+      queryString.split("&").forEach((param) => {
+        const [key, value] = param.split("=");
+        querryParams[key] = value;
+      });
+    }
+
+    console.log(querryParams);
+
     let file;
-
     try {
-      file = await open(`./storage${decodeURIComponent(req.url)}`, "r");
+      file = await open(`./storage${decodeURIComponent(url)}`, "r");
       const stats = await file.stat();
-
       if (stats.isDirectory()) {
         await file.close();
         return await severDirectory(req, res, BoilerplateCode);
+      }
+
+      res.setHeader("Content-Type", mime.lookup(url));
+      res.setHeader("Content-Length", stats.size);
+
+      if (querryParams.action === "download") {
+        res.setHeader(
+          "Content-Disposition",
+          `attachment; filename="${url.slice(1)}"`,
+        );
       }
 
       const fileData = await file.readFile();
