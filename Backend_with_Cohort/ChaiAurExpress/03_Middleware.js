b751bf3c77564c03f4c1b6f0b15a5924aea1e0f4
SumitSharma <sumitkumarsharma2748@gmail.com>
2026-04-28 02:19:56 +0530

    Learn more about express

--- Backend_with_Cohort/ChaiAurExpress/03_Middleware.js
+++ Backend_with_Cohort/ChaiAurExpress/03_Middleware.js
@@ -4,9 +4,78 @@ function block_2_Server() {
     const app = express();
     app.use(express.json());
 
+    // request logger ki Aao entery kro jao
+    const log = [];
+
+    app.use((res, req, next) => {
+      let logEntery = `${req.method} : ${req.url}`;
+      log.push(logEntery);
+      console.log(`[LOG] : ${logEntery}`);
+
+      // if hange froever that means if you forget to call next method in middleware
+      next();
+    });
+
+    function authMe(res, req, next) {
+      const token = req.headers["token"];
+
+      if (!token) {
+        return res.json({ message: "unuthroized user" });
+      }
+
+      if (token !== "secret-key") {
+        return res.json({ message: "unuthroized user" });
+      }
+
+      req.user = { id: 1, name: "sumit", role: "software developer" };
+
+      next();
+    }
+
+    function getRole(accessRoles) {
+      return (req, res, next) => {
+        if (!req.user) {
+          return res.json({ error: "User not authenticated for this Action" });
+        }
+
+        let role = Array.isArray(accessRoles) ? accessRoles : [accessRoles];
+
+        if (!role.includes(req.user.role)) {
+          return res.json({ error: "User not authenticated for this Action" });
+        }
+        next();
+      };
+    }
+
+    const controllerHere = () => {};
+    app.get("/route", authMe, getRole("admin"), controllerHere);
+    app.get("/route", authMe, getRole("user"), controllerHere);
+    app.get("/route", authMe, getRole("superAdmin"), controllerHere);
+    app.get("/route", authMe, getRole(["admin", "teacher"]), controllerHere);
+
+    app.use((req, res, next) => {
+      req.startTime = Data.now();
+
+      res.on("finish", () => {
+        const duration = Date.now() - req.startTime;
+        console.log(
+          `[Timer] : ${req.method} - ${req.url} its took ${duration}ms`,
+        );
+      });
+      next();
+    });
+
     const server = app.listen(0, async () => {
       const port = server.address().port;
       const base = `http://127.0.0.1:${port}`;
+
+      try {
+        const res = await fetch(`${base}/all`, {
+          method: "GET",
+        });
+        const respose = await res.json();
+        console.log(respose);
+      } catch (error) {}
     });
   });
 }
--- Backend_with_Cohort/chai.js
+++ Backend_with_Cohort/chai.js
@@ -1,1 +1,35 @@
-console.log("chai code is teching....");
+const routes = {
+  1: {
+    id: 1,
+    trainName: "KathGodan Express",
+    direction: "north",
+  },
+  2: {
+    id: 2,
+    trainName: "Litchvi Express",
+    direction: "north",
+  },
+};
+
+// const data = {
+//     trainName : "Aman",
+//     direction:"north-east"
+// }
+
+const id = 2;
+delete routes[id];
+// console.log("-->",route);
+// let {trainName, direction} = data
+// route = {id:route.id, trainName,direction}
+// routes[route.id] = route
+console.log(routes);
+
+// let indexId = 3
+// const data = Object.values(routes)
+// const newData = {id: indexId++, ...{    trainName: "Litchvi Express", direction: "north"}}
+// routes[newData.id] = newData
+// console.log(routes)
+
+const AccessFor = ["admin", "teacher"];
+
+req.users.includes(AccessFor);
